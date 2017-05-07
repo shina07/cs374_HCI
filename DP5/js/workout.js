39e50eaed901cc14daa2436fd8f7056dcfbaaf51
moonhc <mhc3357@gmail.com>
2017-05-07 22:24:44 +0900

    merge handled

--- DP5/js/workout.js
+++ DP5/js/workout.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
-<<<<<<< HEAD
 	$(".workout_progress").progressbar({
 		value: 45
-=======
-	$("#progressbar").progressbar({
-		value: 37 
->>>>>>> bb0566d50d0b767bec94f0a22284f6a3224ef219
 	});
 });
 
