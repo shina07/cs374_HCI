f89cd69a1afb45bc92869999671c86443baad869
Taesoo Lim <limtsoo.shina@gmail.com>
2017-05-08 10:59:32 +0900

    Load page Added

---
+++ DP5/js/load.js
@@ -0,0 +1,26 @@
+var table = document.getElementById("load_table");
+
+if (table != null) {
+	for (var i = 0; i < table.rows.length; i++) {
+		table.rows[i].cells[0].onclick = function () {
+			load(this);
+		}
+
+		table.rows[i].cells[1].onclick = function () {
+			open_preview(this);
+		}
+	}
+}
+
+function load(plan) {
+	alert(plan.innerHTML);
+}
+
+function open_preview(plan) {
+	document.getElementById("preview").style.width = "100%";
+}
+
+function close_preview() {
+	document.getElementById("preview").style.width = "0%";
+}
+
