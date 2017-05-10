32548bb3190cb46a5257538ef0e82e39257f20bd
moonhc <mhc3357@gmail.com>
2017-05-11 01:56:44 +0900

    workout view updated (edit func)

--- DP5/js/workout.js
+++ DP5/js/workout.js
@@ -18,10 +18,6 @@ function done() {
 }
 
 function timer() {
-	// if the time becomes 00:00,
-	// change status from time-on to time-over
-	console.log(timerID)
-	
 	current_time += 1
 	var min = Math.floor(current_time / 60)
 	min = min < 10 ? "0" + min : min + ""
@@ -38,16 +34,42 @@ function toggle_video() {
 	$("#hide_video").toggle()
 }
 
+function change_workout_val(flag, this_) {
+	// flag = 1  : count_up
+	// flag = -1 : count_down
+	var cur = parseInt($(this_).parent().find('.btn-default').text())
+	cur = flag == 1 ? cur+1 : cur-1;
+	cur = cur < 0 ? 0 : cur;
+	$(this_).parent().find('.btn-default').html("<b>" + cur + "</b>")
+}
+
 function set_click_event() {
-	$('#show_video, #hide_video').on('click', toggle_video)
+	$('#show_video, #hide_video, .glyphicon-play-circle').on('click', toggle_video)
+
 	$('.done').click(function() {
 		done()
 		timerID = setInterval("timer()", 1000)
 	})
+
 	$('#back').click(function() {
 		clearInterval(timerID)
-		current_time = 0;
+		current_time = 0
 		setTimeout(function() {$('#timer').text("00 : 00")}, 300)
+	})
+
+	$('#next').click(function() {
+		clearInterval(timerID)
+		current_time = 0
+		setTimeout(function() {$('#timer').text("00 : 00")}, 300)
+		location.reload()
+	})
+
+	$('.count_up').click(function(e) {
+		change_workout_val(1, this)
+	})
+
+	$('.count_down').click(function(e) {
+		change_workout_val(-1, this)
 	})
 }
 
@@ -56,6 +78,6 @@ function get_from_firebase(user_info, depth, data_form) {
 }
 
 var timerID = -1
-var current_time = 0;
+var current_time = 0
 $(document).ready(ready_func)
 console.log("asdf")
